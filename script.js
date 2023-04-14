db63ea60d21605f9f94f03a8252deac933e40cca
Jnewton2023 <jonathannewton8@gmail.com>
2023-04-14 12:01:36 -0400

    adding Mod3

---
+++ script.js
@@ -0,0 +1,7 @@
+//Create variables for each vehicle, make, and model
+var carMake="Honda";
+var carModel="Accord";
+var carYear= 2018;
+//Adding vehicle details to alert
+alert (`You won a ${carYear} ${carMake} ${carModel}!!!!🎊🎉🥳` );
+let 
